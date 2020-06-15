d6a9acce89b4318c65f066418401705bd5c20f68
Dima Gerasimov <karlicoss@gmail.com>
2020-06-15 23:49:40 +0100

    foursquare: make defensive

--- foursquare.py
+++ foursquare.py
@@ -2,18 +2,26 @@
 '''
 TODO: use proper data access layer architecture..
 '''
+import json
 import sys
 import requests
-import json
+
 
 LIMIT = 250 # according to https://developer.foursquare.com/docs/users/checkins
 URL = 'https://api.foursquare.com/v2/users/self/checkins?limit={}&oauth_token={}&v=20161020&offset={}'
 
-def run(limit=LIMIT):
-    # Visit https://developer.foursquare.com/docs/explore to generate an OAuth token. Paste that token below.
+
+class RetryMe(Exception):
+    pass
+
+# todo make imports defensive?
+from tenacity import retry, retry_if_exception_type, wait_exponential
+
+@retry(retry=retry_if_exception_type(RetryMe), wait=wait_exponential())
+def get_json(limit=LIMIT):
+    # Visit https://developer.foursquare.com/docs/explore to generate an OAuth token
     from foursquare_secrets import token
 
-    
     offset = 0
     data = []
     while True:
@@ -21,6 +29,9 @@ def run(limit=LIMIT):
         resp = jj['response']
         if 'checkins' not in resp:
             json.dump(jj, sys.stderr, indent=1, ensure_ascii=False)
+            if jj.get('meta', {}).get('errorType') == 'server_error':
+                # started happening around june 2020 for no reason.. seems transient
+                raise RetryMe
             sys.exit(1)
 
         items = len(resp['checkins']['items'])
@@ -28,12 +39,12 @@ def run(limit=LIMIT):
             break
         data.append(jj)
         offset += items
-
-    json.dump(data, sys.stdout, indent=1, ensure_ascii=False, sort_keys=True)
+    return data
 
 
 def main():
-    run()
+    data = get_json()
+    json.dump(data, sys.stdout, indent=1, ensure_ascii=False, sort_keys=True)
 
 
 if __name__ == '__main__':
